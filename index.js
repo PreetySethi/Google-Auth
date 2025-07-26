9dd236d8ddcfa87ff2bf2a8272abeca37a2b57dd
Preety <sethy.preety@gmail.com>
2025-07-26 20:11:34 +0400

    Basic Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+require('./auth');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  res.render('home', { user: req.user });
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/')
+);
+
+app.get('/logout', (req, res) => {
+  req.logout(() => {
+    res.redirect('/');
+  });
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
